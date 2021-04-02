aa7e89fda6bae26383956b4809f1281bbcb87c2e
Arkady Dolina <arkady.dolina@shawagent.com>
2021-04-02 14:14:13 -0400

    ✨init ✨

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { observer, useLocalStore, useObserver } from "mobx-react";
 
-function App() {
+const StoreContext = React.createContext();
+
+const StoreProvider = ({ children }) => {
+  const store = useLocalStore(() => ({
+    bugs: ["Cricket", "LadyBug"],
+    add: (newbug) => {
+      store.bugs.push(newbug);
+    },
+    get bugsCount() {
+      return store.bugs.length;
+    },
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreContext.Provider value={store}>{children} </StoreContext.Provider>
+  );
+};
+
+// const BugsHeader = () => {
+//   const store = React.useContext(StoreContext);
+//   return useObserver(() => <h4>I'm aware of {store.bugsCount} bugs</h4>);
+// };
+
+// alternatively
+const BugsHeader = observer(() => {
+  const store = React.useContext(StoreContext);
+  return <h4>I'm aware of {store.bugsCount} bugs</h4>;
+});
+
+const BugsList = () => {
+  const store = React.useContext(StoreContext);
+  return useObserver(() => (
+    <ul>
+      {store.bugs.map((bug) => (
+        <li key={bug}>{bug}</li>
+      ))}
+    </ul>
+  ));
+};
+
+const BugsInput = () => {
+  const store = React.useContext(StoreContext);
+  const [bug, setBug] = React.useState("");
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="add bug..."
+        value={bug}
+        onChange={(e) => {
+          setBug(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          store.add(bug);
+          setBug("");
+        }}
+      >
+        Submit
+      </button>
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <StoreProvider>
+      <main>
+        <h1>Bugs!</h1>
+        <BugsHeader />
+        <BugsList />
+        <BugsInput />
+      </main>
+    </StoreProvider>
   );
 }
-
-export default App;
